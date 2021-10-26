f0ff612449b35b297b7e40ff4d4b45422b4284da
gjagudeloaunad <gjagudeloa@unadvirtual.edu.co>
2021-10-26 18:32:02 -0500

    correcciones finales
    correcciones finales

--- fase3/index.js
+++ fase3/index.js
@@ -1,10 +1,34 @@
 const Index = {
     init: function() {
+        this.setMenuTemas();
+        this.setMenuRecursos();
+        this.setMenuAprendizaje();
+    },
+
+    setMenuTemas: function() {
+        document.querySelector('#temas').addEventListener('mouseover', e => {
+            document.querySelector('#temas .submenu').style.display = 'flex';
+        });
+        document.querySelector('#temas').addEventListener('mouseleave', e => {
+            document.querySelector('#temas .submenu').style.display = 'none';
+        });
+    },
+
+    setMenuRecursos: function() {
+        document.querySelector('#recursos').addEventListener('mouseover', e => {
+            document.querySelector('#recursos .submenu').style.display = 'flex';
+        });
+        document.querySelector('#recursos').addEventListener('mouseleave', e => {
+            document.querySelector('#recursos .submenu').style.display = 'none';
+        });
+    },
+
+    setMenuAprendizaje: function() {
         document.querySelector('#aprendizaje').addEventListener('mouseover', e => {
-            document.querySelector('.submenu').style.display = 'flex';
+            document.querySelector('#aprendizaje .submenu').style.display = 'flex';
         });
         document.querySelector('#aprendizaje').addEventListener('mouseleave', e => {
-            document.querySelector('.submenu').style.display = 'none';
+            document.querySelector('#aprendizaje .submenu').style.display = 'none';
         });
     }
 }
